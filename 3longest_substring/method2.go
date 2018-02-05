a5f59c091db0394ea33c7c2a05db68046ee33fa3
li.p <li.p@angaomeng.com>
2018-02-05 11:48:16 +0800

    test

--- 3longest_substring/method2.go
+++ 3longest_substring/method2.go
@@ -1,7 +1,5 @@
 package longest_substring
 
-import "fmt"
-
 func method2(str string) int {
 	runes := []rune(str);
 	runeMap := make(map[rune]int)
@@ -11,18 +9,14 @@ func method2(str string) int {
 	for i, v := range runes {
 		var length int
 		if val, ok := runeMap[v]; !ok || val < i - preLengest {
-			fmt.Println("val :", val, " i: ", i, )
 			length = preLengest + 1;
 
 		} else {
-			fmt.Println("val :", val, " i: ", i)
 			length = i - val
 		}
-
 		if length > longest {
 			longest = length
 		}
-
 		preLengest = length
 		runeMap[v] = i
 	}
