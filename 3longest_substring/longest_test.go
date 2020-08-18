1817d08b425155147e8463d766ac61895b66cc3a
lpxxn <mi_duo@live.com>
2020-08-18 12:22:04 +0800

    test

--- 3longest_substring/longest_test.go
+++ 3longest_substring/longest_test.go
@@ -5,7 +5,17 @@ import (
 	"fmt"
 )
 
+
 func Test_longest(t *testing.T) {
+	fmt.Println(longestSubString("abcabbzycdebfdb"))
+	fmt.Println(longestSubString("a"))
+	fmt.Println(longestSubString("aaaa"))
+	fmt.Println(longestSubString("ab"))
+	fmt.Println(longestSubString("dvdf"))
+	fmt.Println(longestSubString("pwwkew"))
+}
+
+func Test_longest1(t *testing.T) {
 	fmt.Println(lengthOfLongestSubstring("abcabbcbb"))
 	fmt.Println(lengthOfLongestSubstring("a"))
 	fmt.Println(lengthOfLongestSubstring("ab"))
@@ -13,9 +23,9 @@ func Test_longest(t *testing.T) {
 }
 
 
-func Test_logest2(t *testing.T) {
-
-	fmt.Println(method2("abcabbcbb"))
+func Test_longest2(t *testing.T) {
+	fmt.Println(method2("abcabbzycdebfdb"))
+	fmt.Println(method2("abcaefghcdebfdb"))
 	fmt.Println(method2("a"))
 	fmt.Println(method2("ab"))
 	fmt.Println(method2("dvdf"))
--- 3longest_substring/method1.go
+++ 3longest_substring/method1.go
@@ -1,8 +1,31 @@
 package longest_substring
 
 import (
+	"fmt"
 	"strings"
 )
+
+func longestSubString(s string) int {
+	m := [128]int{0}
+	longest := 0
+	left := 0
+	for right := 0; right < len(s); right++ {
+		// s[right] 是新遇到的字母
+		// m 是记录字母对应的新位置，
+		// 所以 m[s[right]] 就是新字母对应的新位置
+		left = max(left, m[s[right]])
+		m[s[right]] = right + 1
+		longest = max(longest, right-left+1)
+	}
+	return longest
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
 
 func lengthOfLongestSubstring(s string) int {
 	var strs string = ""
@@ -11,7 +34,7 @@ func lengthOfLongestSubstring(s string) int {
 		lenStrs := len(strs)
 		value := string(item)
 		if lenStrs > 0 && strings.Contains(strs, value) {
-			if  max < lenStrs {
+			if max < lenStrs {
 				max = lenStrs
 			}
 			index := strings.Index(strs, value)
@@ -26,3 +49,30 @@ func lengthOfLongestSubstring(s string) int {
 	return max
 }
 
+// 这个感觉不好
+func method2(str string) int {
+	runes := []rune(str)
+	runeMap := make(map[rune]int)
+	longest := 0
+	preLongest := 0
+
+	for i, v := range runes {
+		var length int
+		currentV := string(v)
+		// ok = true i - preLongest 是上一个重复的位置abcabbzycdebfdb  每二个c 8 - 3 = 5 也就是b的位置，然后每一个c的位置是 2
+		// 也就是说，最近的一个重复的位置比 c 要大。
+		// 也可以这么说，就是两个相同的字母 每一个c(2)和第二个c(8)之前有没有新的多个重复的字母（a重复 bca b重复 cab b重复 b）
+		if val, ok := runeMap[v]; !ok || val < i-preLongest {
+			fmt.Println(currentV, val)
+			length = preLongest + 1
+		} else {
+			length = i - val
+		}
+		if length > longest {
+			longest = length
+		}
+		preLongest = length
+		runeMap[v] = i
+	}
+	return longest
+}
