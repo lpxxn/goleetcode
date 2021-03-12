1d7e27fd6a3a3da5bdc7340ca74f3551e36beca8
lpxxn <mi_duo@live.com>
2021-03-12 17:12:13 +0800

    test

--- 88mergeSortedArray/merge_array_test.go
+++ 88mergeSortedArray/merge_array_test.go
@@ -9,21 +9,7 @@ func TestMergeArray(t *testing.T) {
 	t.Log(a1)
 }
 
-func mergeArray(nums1 []int, m int, nums2 []int, n int) {
-	m--
-	n--
-	last := len(nums1) - 1
-	for n >= 0 {
-		if m >= 0 && nums1[m] > nums2[n] {
-			nums1[last] = nums1[m]
-			m--
-		} else {
-			nums1[last] = nums2[n]
-			n--
-		}
-		last--
-	}
-}
+
 
 /*
 下面是javascript的
