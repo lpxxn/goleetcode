5b443aeef006cc22ff9dea7a5eb6a425c68e53d8
lpxxn <mi_duo@live.com>
2021-03-12 17:13:13 +0800

    test

--- 88mergeSortedArray/merge_array.go
+++ 88mergeSortedArray/merge_array.go
@@ -1,1 +1,17 @@
 package mergeSortedArray
+
+func mergeArray(nums1 []int, m int, nums2 []int, n int) {
+	m--
+	n--
+	last := len(nums1) - 1
+	for n >= 0 {
+		if m >= 0 && nums1[m] > nums2[n] {
+			nums1[last] = nums1[m]
+			m--
+		} else {
+			nums1[last] = nums2[n]
+			n--
+		}
+		last--
+	}
+}
