e887b68dbf59ac962610b063124a6a0ecd89f9d5
lpxxn <mi_duo@live.com>
2022-04-09 17:58:49 +0800

    feat: bak

--- 3longest_substring/test/m_test.go
+++ 3longest_substring/test/m_test.go
@@ -1,5 +1,10 @@
 package test
 
+import (
+	"fmt"
+	"testing"
+)
+
 /*
 Given a string, find the length of the longest substring without repeating characters.
 
@@ -14,3 +19,40 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
 */
 
 // Given a string, find the length of the longest substring without repeating characters
+
+func TestStr1(t *testing.T) {
+	fmt.Println(lengthOfLongestSubstring("abcabbzycdebfdb"))
+	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
+	fmt.Println(lengthOfLongestSubstring("a"))
+	fmt.Println(lengthOfLongestSubstring("aaa"))
+	fmt.Println(lengthOfLongestSubstring("aaaa"))
+	fmt.Println(lengthOfLongestSubstring("ab"))
+	fmt.Println(lengthOfLongestSubstring("dvdf"))
+	fmt.Println(lengthOfLongestSubstring("pwwkew"))
+}
+
+// 这种写法不错，但执行次数多。
+func lengthOfLongestSubstring(s string) (int, string) {
+	m := [128]int{}
+	left, right, count := 0, 0, 0
+	longest := 0
+	longestLeft, longestRight := 0, 0
+	for right < len(s) {
+		count++
+		if m[s[right]] == 0 {
+			m[s[right]]++
+			right++
+		} else {
+			m[s[left]]--
+			left++
+		}
+		length := right - left
+		if length > longest {
+			longest = length
+			longestLeft = left
+			longestRight = right
+		}
+	}
+	fmt.Println(count)
+	return longest, s[longestLeft:longestRight]
+}
