4087d47b56f74b58393f8b41da8b358081d2c5d5
lpxxn <mi_duo@live.com>
2022-04-14 10:11:15 +0800

    fix: test

---
+++ 4medianoftwosortedarrays/binary_search/binary_search_test.go
@@ -0,0 +1,16 @@
+package binary_search
+
+func binarySearch(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return -1
+}
